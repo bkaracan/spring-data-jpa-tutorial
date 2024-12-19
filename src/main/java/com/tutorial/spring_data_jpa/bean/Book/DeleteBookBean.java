e0374957d9435de80f57029139508244ef35234b
bkaracan-erdemir <burakkaracan94@gmail.com>
2024-12-19 17:29:02 +0300

    ++delete book bean added

---
+++ src/main/java/com/tutorial/spring_data_jpa/bean/Book/DeleteBookBean.java
@@ -0,0 +1,34 @@
+package com.tutorial.spring_data_jpa.bean.Book;
+
+import com.tutorial.spring_data_jpa.entity.Book;
+import com.tutorial.spring_data_jpa.enums.MessageEnum;
+import com.tutorial.spring_data_jpa.enums.ResponseEnum;
+import com.tutorial.spring_data_jpa.payload.AbstractResponsePayload;
+import com.tutorial.spring_data_jpa.payload.ResponsePayload;
+import com.tutorial.spring_data_jpa.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Component
+public class DeleteBookBean extends AbstractResponsePayload {
+
+    private final BookRepository bookRepository;
+
+    @Autowired
+    public DeleteBookBean(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @Transactional
+    public ResponsePayload<?> deleteById(Long id) {
+        Optional<Book> bookOptional = bookRepository.findById(id);
+
+        return bookOptional.map(book -> {
+            bookRepository.delete(book);
+            return createSuccessResponse(MessageEnum.DELETE_SUCCESS.getMessage(), null);
+        }).orElseGet(() -> createErrorResponse(ResponseEnum.NOT_FOUND, MessageEnum.NOT_FOUND.getMessage(), true));
+    }
+}
